8fc584de053fa57d2ad41ff6d4d674601a336cc0
president_business <74831297+mcnortonjosh@users.noreply.github.com>
2021-01-12 21:38:27 -0600

    created logo for dashboard

---
+++ src/assets/icons/lightbrain_negative.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Logo extends React.Component {
+    render() {
+        //JSX
+        return(
+            
+                <img height={100} src="../../../LightBrain_negative.png" alt="ohhh hellooo!"/>
+           
+        );
+    }
+}
+
+export default Logo;
+
+
+// why are the paths wrong?????
+// C:\Users\Joshua McNorton\Documents\github\reactdashboard\src\assets\icons\lightbrain_negative.js
+// C:\Users\Joshua McNorton\Documents\github\reactdashboard\public\LightBrain_negative.png
